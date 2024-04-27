c086a50bc2d6d2f731c4ca831289e3515cd45028
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-04-27 09:11:39 +0530

    Login Auth Completed

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -1,31 +1,54 @@
 import React, { useState } from 'react'
 import "./Login.css"
+import { useNavigate } from 'react-router-dom'
+import { createClient } from '@supabase/supabase-js'
+
+
+const supabase = createClient('https://jqsqjrcaxnxjrqennhmk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxc3FqcmNheG54anJxZW5uaG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1MTI3MjEsImV4cCI6MjAyOTA4ODcyMX0.19Y3Zjd72vKn6S9Us2djfjj7BUDSkkqBwbu6j8y2ug4')
 
 function Login() {
 
-    const [ email, setEmail] = useState('')
-    const [ password, setPassword] = useState('')
+    const navigate = useNavigate()
 
-    function handleEmail(e){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    // let user = false
+    // const [ token, setToken] = useState();
+
+    function handleEmail(e) {
         setEmail(e.target.value)
     }
 
-    function handlePassword(e){
+    function handlePassword(e) {
         setPassword(e.target.value)
     }
 
-  return (
-    <div>
-        <div className='content-container'>
-            <div className='signin-container'>
-                <div className='sign-in-text'>Sign In</div>
-                <input type='text' onChange={handleEmail} value={email} placeholder='Email or Phone'></input>
-                <input type='password' onChange={handlePassword} value={password} placeholder='Password'></input>
-                <button type='submit' >LOGIN</button>
+    async function handleLogin() {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+
+        if(!data.user){
+            alert('Invalid login credentials')
+        }
+        else{
+            navigate("/todo")
+        }
+    }
+
+    return (
+        <div>
+            <div className='content-container'>
+                <div className='signin-container'>
+                    <div className='sign-in-text'>Sign In</div>
+                    <input type='text' onChange={handleEmail} value={email} placeholder='Email'></input>
+                    <input type='password' onChange={handlePassword} value={password} placeholder='Password'></input>
+                    <button type='submit' onClick={handleLogin} >LOGIN</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
